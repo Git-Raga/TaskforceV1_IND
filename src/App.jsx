70cfb6700cf0592f8cde144526cb9d3b495b452b
Raghavendran <34478862+Git-Raga@users.noreply.github.com>
2025-02-01 09:49:35 +0530

    footer removal

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ function App() {
             } 
           />
         </Routes>
-        <Footer /> {/* Render the Footer component */}
+        
       </BrowserRouter>
     </AuthProvider>
   );
