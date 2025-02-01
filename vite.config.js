dfd8259a98d187979558259722fa06fbe6810a80
Raghavendran <34478862+Git-Raga@users.noreply.github.com>
2025-02-01 10:34:33 +0530

    vite config 2

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 base: '/',
+server: {
+  historyApiFallback: true
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
