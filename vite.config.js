fb2887c8e6d413d282e18faa6275495c8c80cb09
Raghavendran <34478862+Git-Raga@users.noreply.github.com>
2025-02-01 10:37:15 +0530

    vercel config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-base: '/',
-server: {
-  historyApiFallback: true
-}
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  server: {
+    historyApiFallback: true
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
